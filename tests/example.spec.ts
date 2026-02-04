fb8067a1f2d61f77349e805c7bf4f32f63918cfa
iyoshi-rgb <yoshito.i.2002@gmail.com>
2026-02-04 11:53:44 +0900

    playwrightでCI実行5

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -5,8 +5,6 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
-
-  await expect(page.getByText('ABCDEFG')).toBeVisible();
 });
 
 test('get started link', async ({ page }) => {
