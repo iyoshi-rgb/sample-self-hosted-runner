b849614083dd7f481a3906598b8297ab27c75b3f
iyoshi-rgb <yoshito.i.2002@gmail.com>
2026-02-04 22:51:48 +0900

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -46,6 +46,11 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
