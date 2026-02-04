dca701e252981846570c3d39bdc045ee86740851
iyoshi-rgb <yoshito.i.2002@gmail.com>
2026-02-04 11:36:52 +0900

    playwrightでCI実行3 失敗させる

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -6,6 +6,8 @@ test('has title', async ({ page }) => {
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+
+  await expect(page.getByText('ABCDEFG')).toBeVisible();
 });
 
 test('get started link', async ({ page }) => {
