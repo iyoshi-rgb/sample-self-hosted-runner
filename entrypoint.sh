4eaa8021acec5cc1319b977b37282cd681e7875f
iyoshi-rgb <yoshito.i.2002@gmail.com>
2026-02-04 12:25:17 +0900

    TOKENを動的に取得するように修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e
 
-# 環境変数チェック
+RUNNER_TOKEN=$(curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${PAT_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/iyoshi-rgb/${GITHUB_REPOSITORY}/actions/runners/registration-token \
+  | jq -r '.token')
+
 if [[ -z "${GITHUB_REPOSITORY:-}" || -z "${RUNNER_TOKEN:-}" ]]; then
-  echo "Error: GITHUB_REPOSITORY と RUNNER_TOKEN を設定してください" >&2
+  echo "Error: GITHUB_REPOSITORYを設定してください" >&2
   exit 1
 fi
 
 GITHUB_URL="https://github.com/iyoshi-rgb/${GITHUB_REPOSITORY}"
 
-./config.sh --url "${GITHUB_URL}" --token "${RUNNER_TOKEN}" --ephemeral
+./config.sh --url "${GITHUB_URL}" --token "${RUNNER_TOKEN}" --ephemeral --unattended
 
 ./run.sh
